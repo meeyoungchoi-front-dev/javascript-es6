b2bf96bbd520a23ceeec6cb76a89b66970378509
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-04 10:24:38 +0900

    ㅅ스프레드 오퍼레이터

---
+++ spread-operator/script.js
@@ -0,0 +1,16 @@
+var arr = ['a', 'b'];
+
+var newArr = [arr[0], arr[1]];
+
+var newArr = [...arr, 'c'];
+
+var newArr =[...arr];
+
+var user = {
+    b: obj.a,
+    a: obj.a,
+    c: 30
+
+    
+};
+
