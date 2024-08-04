96ee84e22060a932b06f5d2bc02e0d69ef8d8135
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-04 10:09:21 +0900

    자바스크립트 es6 새로운 문법 탬플릿 리터럴, 구조분해할당, 스프레드 오퍼레이터, 디스틑럭쳐링

---
+++ destructing/script2.js
@@ -0,0 +1,59 @@
+`var arr = ['apple', 10];
+
+var favoriteFruite = arr[0];
+var myNumber = arr[1];
+
+var [favoriateFruite, myNum] = arr;
+
+var josh = {
+    skill: 'js',
+    age: 21    
+}
+
+var mySkill = josh.skill;
+var myAge = josh.age;
+
+var {skill, age} = josh;
+
+var {skill: mySkill, age: myAge} = josh;
+
+var obj = {
+    a: 10,
+    b: 20,
+}
+
+var user = {
+    a: obj.a'
+    b: oaj.b;
+}
+var user = {
+    b: obj.a;
+    a: obj.a;
+    c: 30;
+};
+
+var user = {
+    b: obj.a,
+    a: obj.a,
+    c: 30;
+};
+
+var user = {
+    b: obj.a,
+    a: obj.a,
+    c: 30
+};
+
+var user = {...ob,,
+            c: 30,
+           };
+
+var newArr = [...arr, 'c'];
+
+
+varr arr = ['a', 'b'];
+
+var arr = ['a', 'b'];
+
+var newArr = [arr[0], arr[1]];
+var newArr =[...arr];
---
+++ template-literal/script.js
@@ -0,0 +1,10 @@
+var a = 'h1';
+
+var a = 'h1';
+
+var c = `hi`;
+
+var message = `{a} 캡팡`;
+
+message = a + ` 캡팡`;
+
