37596d9f08df2cda54464482033510b46de77b56
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-05 09:38:03 +0900

    lodash 라이브러리를 사용하여 자바스크립트 파일 import, export 하는 방법 실습

---
+++ import-export-module/app.js
@@ -0,0 +1,3 @@
+let message = "hi";
+
+export default message; // 파일 안에서 내보낼게 하나다 라고 알려준다
---
+++ import-export-module/import.mjs
@@ -0,0 +1,12 @@
+// 일반 문법
+import { pi, sum } from "./math";
+// default export 문법
+import message from "./app";
+
+// import 라이브러리이름 from "라이브러리 이름"
+import lodashLibray from "lodash";
+
+console.log(pi);
+console.log(sum(10, 20));
+console.log(message);
+console.log(lodashLibray.VERSION);
