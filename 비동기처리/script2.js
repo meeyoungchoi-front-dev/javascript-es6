decdc336afdacbe89a0a67d3d3be9d1271ba30c5
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-06 15:34:12 +0900

    비동기처리 예시 코드로 비동기 처리가 필요한 이유와 동자강식 분석

--- 비동기처리/script2.js
+++ 비동기처리/script2.js
@@ -3,13 +3,16 @@
 // 특정 코드의 실행을 기다리지 않고 다른 코드를 실행한다
 // 왜 콜백 함수로 비동기 처리를 해야 하는가
 function fetchMenuInfo() {
-    return axios.get("https://domain.com/api/menu");
+    let menu
+    return axios.get("https://domain.com/api/menu", function (response) {
+        menu = response;
+    });
 }
 
 // 1. 사용자 정보 요청
 fetchuser(); // 0.1s
 // 2. 좌측 메뉴 정보 요청
-const menu = fetchMenuInfo(); // 1시간 => 밑에 있는 3, 4번 코드는 실행이 되지 않는단
+// let menu = fetchMenuInfo(); // 1시간 => 밑에 있는 3, 4번 코드는 실행이 되지 않는단
 // 3, 영상 본문 요청
 fetchContents(); // 1초
 // 4. 페이지 UI 표시
